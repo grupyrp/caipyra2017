37cd7db0be3cf092cf8e8831d31019cb76f91936
Leo Berdu <leoberdu@gmail.com>
2017-05-05 19:20:52 -0300

    remove old files, add new files

--- gulp/tasks/default.js
+++
@@ -1,6 +0,0 @@
-gulp.task('default', ["webserver", "build"], () => {
-    gulp.watch('./src/sass/**/*', ['css', 'mobile_css'])
-    gulp.watch(CONFIG.SRC.JS, ['js'])
-    gulp.watch('./src/templates/**/*', ['templates'])
-    gulp.watch(CONFIG.SRC.IMAGES, ['images'])
-})
--- gulp/tasks/templates.js
+++
@@ -1,6 +0,0 @@
-gulp.task("templates", () => {
-    return gulp.src(CONFIG.SRC.PAGE)
-                .pipe($.compileHandlebars(CONFIG.TASK.TEMPLATE.TMP_DATA, CONFIG.TASK.TEMPLATE.HBS))
-                .pipe($.rename(CONFIG.TASK.TEMPLATE.RENAME))
-                .pipe(gulp.dest(CONFIG.DIST.PAGE))
-})
